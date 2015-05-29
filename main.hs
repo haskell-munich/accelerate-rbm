fe1a460bd97dcefe3df6c24534911376253f2a23
Markus Liedl <m.liedl@gmx.de>
2015-05-29 12:02:48 +0200

    step

--- main.hs
+++ main.hs
@@ -1,31 +1,49 @@
 import Data.Array.Accelerate
-  (fill, constant, Acc, Z(..), (:.)(..), Array, DIM1, DIM2)
+  (fill, constant, Acc, Z(..), (:.)(..),
+   Array, DIM1, DIM2, use)
 import Data.Array.Accelerate.Interpreter as I
 
+-- the weight matrix
 type W = Array DIM2 Float
+
+-- the biases for the visible units
 type V = Array DIM1 Float
+
+-- the biases for the hidden units
 type H = Array DIM1 Float
 
-data RBM = RBM { weights :: Acc W
-               , vbias :: Acc V
-               , hbias :: Acc H }
+data RBM = RBM { weights :: W
+               , vbias :: V
+               , hbias :: H }
+           deriving (Show)
+
+-- the activations of the visible units
+type VAct = Array DIM1 Float
+
+-- the states of the visible units
+type VState = Array DIM1 Bool
+
 
 initialWeights :: Int -> Int -> RBM
 initialWeights nv nh =
-  let w = fill (constant $ Z :. nv :. nh) 0.0 :: Acc W
-      v = fill (constant $ Z :. nv) 0.0 :: Acc V
-      h = fill (constant $ Z :. nh) 0.0 :: Acc H
+  let w = I.run $ fill (constant $ Z :. nv :. nh) 0.0 :: W
+      v = I.run $ fill (constant $ Z :. nv) 0.0 :: V
+      h = I.run $ fill (constant $ Z :. nh) 0.0 :: H
   in RBM w v h
 
-runRBM (RBM w v h) =
-  (I.run w, I.run v, I.run h)
-
 -- sigmoid
+
+hact :: RBM -> VState -> Acc VAct
+hact (RBM w v h) vis =
+  use v
+
 -- propup -- p(h|v)
+-- hact :: RBM -> 
+  
 -- sampleH -- sample from p(h|v)
 -- propdown -- p(v|h)
 -- sampleV -- sample from p(v|h)
 -- cd1
 
 main =
-  print $ runRBM $ initialWeights 3 4
+  print $ initialWeights 3 4
