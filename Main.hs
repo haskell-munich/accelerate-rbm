4b4be8f6cf3303e64dfaf59d43463d72a7a940e2
Markus Liedl <m.liedl@gmx.de>
2015-05-29 17:04:33 +0200

    random generator produces ints

--- Main.hs
+++ Main.hs
@@ -7,6 +7,9 @@ import Data.Array.Accelerate
    transpose, fold, fromList, map, unzip,
    Int64, Int32, lift)
 
+import System.Random(getStdRandom, randomR)
+import Control.Monad(replicateM)
+
 import Data.Bits(Bits((.&.)))
    
 import Data.Array.Accelerate.Interpreter as I
@@ -45,8 +48,8 @@ sigmoid act =
   1 / (1 + exp (-act))
 
 -- propup -- p(h|v)
-propup :: RBM -> VState -> Acc HState
-propup rbm vis =
+propup :: RBM -> VState -> PRNG -> Acc HState
+propup rbm vis prng =
   map sigmoid $ hact rbm vis
 
   
@@ -60,6 +63,13 @@ type IntR = Int64
 type PRNG = Array DIM1 IntR
 type Randoms = Array DIM1 IntR
 
+
+mkPRNG :: Int -> IO PRNG
+mkPRNG size =
+  do let mx = maxBound :: IntR
+     rs <- replicateM size (getStdRandom (randomR (20000, mx)))
+     return $ fromList (Z :. size) rs
+  
 -- Generate numbers from 0 to 1023 and also a new state
 -- that can be used to generate more random numbers.
 randoms :: Acc PRNG -> (Acc PRNG, Acc Randoms)
@@ -74,18 +84,15 @@ randoms state = (map next state,
 testRandoms :: IO ()
 testRandoms =
   do let n = 3
-         r0 = fromList (Z :. n) [123678, 123789, 234890]
-              :: Array DIM1 IntR
-         (r1, as) = randoms (use r0)
-         (r2, bs) = randoms r1
-         (r3, cs) = randoms r2
-         (r4, ds) = randoms r3
-         (r5, es) = randoms r4
-     print (I.run as)
-     print (I.run bs)
-     print (I.run cs)
-     print (I.run ds)
-     print (I.run es)
+     let f _r 0 = return ()
+         f r0 num =
+           do let (r1, as) = randoms $ use r0
+              print (I.run as)
+              f (I.run r1) (pred num)
+     r0 <- mkPRNG n
+     print("r0", r0)
+     f r0 1000
+           
   
   
 testRBM =
@@ -93,8 +100,10 @@ testRBM =
       rbm = initialWeights nv nh
       v1 = fromList (Z :. nv) [0,1,1] :: VState
       h1act = I.run $ hact rbm v1
+      -- h1s = I.run $ propup rbm v1
   in
    print h1act
+   --print h1s
 
 
 
